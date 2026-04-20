1650a2db42056478ccfd80e48a0c232cfb981eae
Vini Souza <vinicius.souza@meutudo.app>
2026-04-19 21:28:42 -0300

    feat(shared): add TopMaterial type

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export * from './enums/roles.enum';
 export * from './enums/tenant-status.enum';
 export * from './enums/tenant-segment.enum';
 export * from './enums/cash-register.enums';
+export * from './types/top-material';
